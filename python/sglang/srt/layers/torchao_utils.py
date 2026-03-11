93666a4b4ab381e6c6b52266e82745750f9b0816
DefTruth <qiustudent_r@163.com>
2026-03-11 08:43:34 +0000

    Bump up torchao to 0.16.0

--- python/sglang/srt/layers/torchao_utils.py
+++ python/sglang/srt/layers/torchao_utils.py
@@ -46,35 +46,23 @@ def apply_torchao_config_to_model(
         return model
 
     # Lazy import to suppress some warnings
-    try:
-        # Before v0.15.0, we can import the config functions directly.
-        from torchao.quantization import (
-            float8_dynamic_activation_float8_weight,
-            float8_weight_only,
-            int4_weight_only,
-            int8_dynamic_activation_int8_weight,
-            int8_weight_only,
-            quantize_,
-        )
-    except ImportError:
-        # After v0.15.0, torchao removed config functions like 'int4_weight_only'.
-        # We have to use the config class directly, e.g, 'Int4WeightOnlyConfig'.
-        # Reference: https://github.com/pytorch/ao/releases/tag/v0.15.0
-        from torchao.quantization import (
-            Float8DynamicActivationFloat8WeightConfig as float8_dynamic_activation_float8_weight,
-            Float8WeightOnlyConfig as float8_weight_only,
-            Int4WeightOnlyConfig as int4_weight_only,
-            Int8DynamicActivationInt8WeightConfig as int8_dynamic_activation_int8_weight,
-            Int8WeightOnlyConfig as int8_weight_only,
-            quantize_,
-        )
-
+    # After v0.15.0, torchao removed config functions like 'int4_weight_only'.
+    # We have to use the config class directly, e.g, 'Int4WeightOnlyConfig'.
+    # Reference: https://github.com/pytorch/ao/releases/tag/v0.15.0
+    from torchao.quantization import (
+        Float8DynamicActivationFloat8WeightConfig,
+        Float8WeightOnlyConfig,
+        Int4WeightOnlyConfig,
+        Int8DynamicActivationInt8WeightConfig,
+        Int8WeightOnlyConfig,
+        quantize_,
+    )
     from torchao.quantization.observer import PerRow, PerTensor
 
     if "int8wo" in torchao_config:
-        quantize_(model, int8_weight_only(), filter_fn=proj_filter_conv3d)
+        quantize_(model, Int8WeightOnlyConfig(), filter_fn=proj_filter_conv3d)
     elif "int8dq" in torchao_config:
-        quantize_(model, int8_dynamic_activation_int8_weight(), filter_fn=filter_fn)
+        quantize_(model, Int8DynamicActivationInt8WeightConfig(), filter_fn=filter_fn)
     elif "int4wo" in torchao_config:
         group_size = int(torchao_config.split("-")[-1])
         assert group_size in [
@@ -83,11 +71,13 @@ def apply_torchao_config_to_model(
             128,
             256,
         ], f"int4wo groupsize needs to be one of [32, 64, 128, 256] but got {group_size}"
-        quantize_(model, int4_weight_only(group_size=group_size), filter_fn=filter_fn)
+        quantize_(
+            model, Int4WeightOnlyConfig(group_size=group_size), filter_fn=filter_fn
+        )
     elif "fp8wo" in torchao_config:
         # this requires newer hardware
         # [rank0]: AssertionError: fp8e4nv data type is not supported on CUDA arch < 89
-        quantize_(model, float8_weight_only(), filter_fn=proj_filter_conv3d)
+        quantize_(model, Float8WeightOnlyConfig(), filter_fn=proj_filter_conv3d)
     elif "fp8dq" in torchao_config:
         granularity = torchao_config.split("-")[-1]
         GRANULARITY_MAP = {
@@ -99,7 +89,7 @@ def apply_torchao_config_to_model(
         ), f"Supported granularity are: {GRANULARITY_MAP.keys()}, got {granularity}"
         quantize_(
             model,
-            float8_dynamic_activation_float8_weight(
+            Float8DynamicActivationFloat8WeightConfig(
                 granularity=GRANULARITY_MAP[granularity]
             ),
             filter_fn=proj_filter_conv3d,
